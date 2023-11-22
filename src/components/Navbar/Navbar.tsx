7cfde9da118e9dcdf7c11ffae99843f3b859bc81
Zdeněk Němec <xzdeneknemec@outlook.cz>
2023-11-22 15:38:41 +0100

    add home page logo, tab favicon, create new pages, add hover effect to navbar, remove header title

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,30 +1,35 @@
 import React, {ReactElement} from "react";
-import './Navbar.module.scss';
+import styles from './Navbar.module.scss';
+import logo from '../../images/logos/logo.png'
 
 export default function Navbar(): ReactElement {
     return (
-        <nav className="nav">
-            <a href="/" className="siteTitle">Aparatea</a>
-            <ul>
-                <li>
-                    <a href="/home">DOMŮ</a>
-                </li>
-                <li>
-                    <a href="/about">O NÁS</a>
-                </li>
-                <li>
-                    <a href="/career">VOLNÁ MÍSTA</a>
-                </li>
-                <li>
-                    <a href="/references">REFERENCE</a>
-                </li>
-                <li>
-                    <a href="/contact">KONTAKT</a>
-                </li>
-                <li>
-                    <a href="/contactus">NAPIŠTE NÁM</a>
-                </li>
-            </ul>
-        </nav>
+        <div>
+            <nav className={styles.nav}>
+                <a href="/">
+                    <img src={logo} alt="logo" className={styles.siteLogo} width='200' height='auto'/>
+                </a>
+                <ul>
+                    <li>
+                        <a href="/home">DOMŮ</a>
+                    </li>
+                    <li>
+                        <a href="/about">O NÁS</a>
+                    </li>
+                    <li>
+                        <a href="/career">VOLNÁ MÍSTA</a>
+                    </li>
+                    <li>
+                        <a href="/references">REFERENCE</a>
+                    </li>
+                    <li>
+                        <a href="/contact">KONTAKT</a>
+                    </li>
+                    <li>
+                        <a href="/contactus">NAPIŠTE NÁM</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
     )
 }
