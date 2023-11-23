0d14aa1c853a2f64d1988ffa1a0d6e59c697b627
Zdeněk Němec <xzdeneknemec@outlook.cz>
2023-11-23 10:44:21 +0100

    refactor(Styles): use usual approach instead of modules; refactor(Navbar): use hamburger when too small, else display flex, fix paddings and margins

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,20 +1,60 @@
-import React, {ReactElement} from "react";
-import styles from './Navbar.module.scss';
+import React, {ReactElement, useEffect, useState} from "react";
+import './Navbar.scss';
 import logo from '../../images/logos/logo.png'
 
 export default function Navbar(): ReactElement {
+
+    // const [menuActive, setMenuActive] = useState(false);
+    //
+    // const handleToggleClick = () => {
+    //     console.log(menuActive);
+    //     setMenuActive(!menuActive);
+    // };
+
+    useEffect(() => {
+        const menuToggle: Element | null = document.querySelector('.menu-toggle');
+        const menu: Element | null = document.querySelector('.menu');
+
+        const handleToggleClick = () => {
+            if (menuToggle && menu) {
+                menuToggle.classList.toggle('active');
+                menu.classList.toggle('active');
+            }
+        };
+
+        if (menuToggle) {
+            menuToggle.addEventListener('click', handleToggleClick);
+        }
+
+        return () => {
+            // Cleanup: Remove the event listener when the component unmounts
+            if (menuToggle) {
+                menuToggle.removeEventListener('click', handleToggleClick);
+            }
+        }
+    }, []);
+
     return (
         <div>
-            <nav className={styles.nav}>
+            <nav className="nav">
                 <a href="/">
-                    <img src={logo} alt="logo" className={styles.siteLogo} width='200' height='auto'/>
+                    <img src={logo} alt="logo" className="siteLogo" width='200' height='auto'/>
                 </a>
-                <ul className={styles.navLinks}>
+                <div className="menu-toggle">
+                    <input type="checkbox" className="bar"/>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+                <ul className="menu">
                     <li>
                         <a href="/home">DOMŮ</a>
                     </li>
                     <li>
                         <a href="/about">O NÁS</a>
+                    </li>
+                    <li>
+                        <a href="/e-shops">NAŠE E-SHOPY</a>
                     </li>
                     <li>
                         <a href="/career">VOLNÁ MÍSTA</a>
@@ -24,9 +64,6 @@ export default function Navbar(): ReactElement {
                     </li>
                     <li>
                         <a href="/contact">KONTAKT</a>
-                    </li>
-                    <li>
-                        <a href="/contactus">NAPIŠTE NÁM</a>
                     </li>
                 </ul>
             </nav>
