b3b00c5187398cd6ae964d1e2dec59aa87eddccc
Zdeněk Němec <xzdeneknemec@outlook.cz>
2023-11-24 13:27:49 +0100

    chore(Pages:About): completely change about content layout, fix global background

--- src/pages/Eshops/Eshops.tsx
+++ src/pages/Eshops/Eshops.tsx
@@ -6,14 +6,22 @@ export default function Eshops() {
             <a href="https://www.stopkovefrezy.cz/" rel="noreferrer noopener" target="_blank" className="eshops-content-text">
                 <h5>stopkovefrezy.cz</h5>
                 <h3>Stopkové frézy</h3>
-                <p>Naše firma vznikla v roce 1996 a již od této doby se <b>odborně věnujeme distribuci a především poradenství ohledně profesionálních nástrojů pro zpracování dřeva</b>, materiálů na bázi dřeva a plastů od renomovaných výrobců.</p>
-                <p>Již roky <b className="">spolupracujeme s velkým počtem odborných firem</b> zabývajících se zpracováním dřeva, a to nejen v České republice, ale v celé Evropě. Poslední <b className="">roky expandujeme </b>a snažíme se pro Vás zlepšovat náš servis a dostupnost, abychom Vám mohli být na blízko v každé situaci.</p>
+                <p>Když potká sob soba,
+                    mají radost oba.
+                    Když se sejdou tři sobi,
+                    tak se radost násobí.
+                    A když se sejdou čtyři sobi,
+                    tak si vlezou do hospody.</p>
             </a>
             <a href="https://www.itatools.cz/" rel="noreferrer noopener" target="_blank" className="eshops-content-text">
                 <h5>itatools.cz</h5>
                 <h3>ITA TOOLS</h3>
-                <p>Naše firma vznikla v roce 1996 a již od této doby se <b>odborně věnujeme distribuci a především poradenství ohledně profesionálních nástrojů pro zpracování dřeva</b>, materiálů na bázi dřeva a plastů od renomovaných výrobců.</p>
-                <p>Již roky <b className="">spolupracujeme s velkým počtem odborných firem</b> zabývajících se zpracováním dřeva, a to nejen v České republice, ale v celé Evropě. Poslední <b className="">roky expandujeme </b>a snažíme se pro Vás zlepšovat náš servis a dostupnost, abychom Vám mohli být na blízko v každé situaci.</p>
+                <p>Když potká sob soba,
+                    mají radost oba.
+                    Když se sejdou tři sobi,
+                    tak se radost násobí.
+                    A když se sejdou čtyři sobi,
+                    tak si vlezou do hospody.</p>
             </a>
         </div>
     )
