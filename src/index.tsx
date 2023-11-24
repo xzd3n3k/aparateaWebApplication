ad99db68efb672ecfc3418d96fbf973ebd6957f5
Zdeněk Němec <xzdeneknemec@outlook.cz>
2023-11-24 09:08:46 +0100

    fix navbar, routing, add content to about page

--- src/index.tsx
+++ src/index.tsx
@@ -15,7 +15,7 @@ root.render(
             <div>
                 <Navbar />
                 <Routes>
-                    <Route path="/home" element={<Home />} />
+                    <Route path="/" element={<Home />} />
                     <Route path="/about" element={<About />} />
                 </Routes>
             </div>
--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,5 +1,14 @@
+import './About.scss';
+
 export default function About() {
     return (
-        <div>Kdyz potka sob soba, maji radost oba. Kdyz se potkaj tri sobi, tak se radost nasobi. A kdyz se potkaj ctyri tak jdou do hospody.</div>
+        <div className="about-content">
+            <div className="about-content-text">
+                <h5>A něco málo…</h5>
+                <h3>O NÁS</h3>
+                <p>Naše firma vznikla v roce 1996 a již od této doby se <b>odborně věnujeme distribuci a především poradenství ohledně profesionálních nástrojů pro zpracování dřeva</b>, materiálů na bázi dřeva a plastů od renomovaných výrobců.</p>
+                <p>Již roky <b className="">spolupracujeme s velkým počtem odborných firem</b> zabývajících se zpracováním dřeva, a to nejen v České republice, ale v celé Evropě. Poslední <b className="">roky expandujeme </b>a snažíme se pro Vás zlepšovat náš servis a dostupnost, abychom Vám mohli být na blízko v každé situaci.</p>
+            </div>
+        </div>
     )
 }
