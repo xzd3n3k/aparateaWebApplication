4b280f46c97df88a55c82b8b33a673d0067475f1
Zdeněk Němec <xzdeneknemec@outlook.cz>
2023-11-24 13:45:42 +0100

    chore(Pages:About): add content, edit existing content layout

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -11,33 +11,27 @@ export default function About() {
             <div className="about-content">
                 <div className="about-content-text">
                     <p>Naše firma vznikla v roce 1996 a již od této doby se <b>odborně věnujeme distribuci a především poradenství ohledně profesionálních nástrojů pro zpracování dřeva</b>, materiálů na bázi dřeva a plastů od renomovaných výrobců.</p>
-                    <p>Již roky <b className="">spolupracujeme s velkým počtem odborných firem</b> zabývajících se zpracováním dřeva, a to nejen v České republice, ale v celé Evropě. Poslední <b className="">roky expandujeme </b>a snažíme se pro Vás zlepšovat náš servis a dostupnost, abychom Vám mohli být na blízko v každé situaci.</p>
+                </div>
+
+                <div className="about-content-text">
+                    <p>Již roky <b className="">spolupracujeme s velkým počtem odborných firem</b> zabývajících se zpracováním dřeva, a to nejen v České republice, ale v celé Evropě.</p>
+                    <p>Poslední <b className="">roky expandujeme </b>a snažíme se pro Vás zlepšovat náš servis a dostupnost, abychom Vám mohli být na blízko v každé situaci.</p>
                 </div>
 
                 <div className="about-content-text">
                     <p>V reakci na tržní poptávku <b>neustále rozšiřujeme naši nabídku</b>, abychom našim klientům pomohli <b>optimalizovat výrobní proces a snížit jeho náklady</b>. Na zvláštní žádost poskytujeme také nástroje s <b>neobvyklými rozměry a aplikacemi</b>.</p>
                     <p>Naši <b>kvalifikovaní zaměstnanci</b> poskytují <b>technické poradenství</b> při výrobě vhodných nástrojů a jejich údržbě.</p>
+                </div>
+
+                <div className="about-content-text">
                     <p>Po celá léta se <b>aktivně účastníme domácích a zahraničních veletrhů</b> a navazujeme obchodní kontakty, které nám umožňují dynamicky rozvíjet prodej nejen v <b>Polsku, ale také třeba v České republice</b> či jiných státech.</p>
                 </div>
 
                 <div className="about-content-text">
-                    <p>Již několik let je náš stánek oceněn cenou Acanthus Aureus. Soška se uděluje subjektům, jejichž postavení nejlépe odráží marketingovou strategii společnosti.</p>
+                    <p><b>Nejvýznamnější událostí</b> každého roku je <b>mezinárodní veletrh strojů a nářadí DREMA</b> pro dřevařský a nábytkářský průmysl v Poznani. Jsme hrdí na to, že můžeme představit další <b>nové značky</b>, které se připojily k našemu portfoliu produktů.</p>
+                    <p>Již několik let je náš stánek <b>oceněn cenou Acanthus Aureus</b>. Soška se uděluje subjektům, jejichž postavení <b>nejlépe odráží marketingovou strategii společnosti</b>.</p>
                 </div>
 
-                <div className="about-content-text">
-                    <p>Naše firma vznikla v roce 1996 a již od této doby se <b>odborně věnujeme distribuci a především poradenství ohledně profesionálních nástrojů pro zpracování dřeva</b>, materiálů na bázi dřeva a plastů od renomovaných výrobců.</p>
-                    <p>Již roky <b className="">spolupracujeme s velkým počtem odborných firem</b> zabývajících se zpracováním dřeva, a to nejen v České republice, ale v celé Evropě. Poslední <b className="">roky expandujeme </b>a snažíme se pro Vás zlepšovat náš servis a dostupnost, abychom Vám mohli být na blízko v každé situaci.</p>
-                </div>
-
-                <div className="about-content-text">
-                    <p>V reakci na tržní poptávku <b>neustále rozšiřujeme naši nabídku</b>, abychom našim klientům pomohli <b>optimalizovat výrobní proces a snížit jeho náklady</b>. Na zvláštní žádost poskytujeme také nástroje s <b>neobvyklými rozměry a aplikacemi</b>.</p>
-                    <p>Naši <b>kvalifikovaní zaměstnanci</b> poskytují <b>technické poradenství</b> při výrobě vhodných nástrojů a jejich údržbě.</p>
-                    <p>Po celá léta se <b>aktivně účastníme domácích a zahraničních veletrhů</b> a navazujeme obchodní kontakty, které nám umožňují dynamicky rozvíjet prodej nejen v <b>Polsku, ale také třeba v České republice</b> či jiných státech.</p>
-                </div>
-
-                <div className="about-content-text">
-                    <p>Již několik let je náš stánek oceněn cenou Acanthus Aureus. Soška se uděluje subjektům, jejichž postavení nejlépe odráží marketingovou strategii společnosti.</p>
-                </div>
             </div>
         </div>
     )
