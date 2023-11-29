8bd9ad2f1ea1fad17cd51bcddd655985ec286cb5
Zdeněk Němec <xzdeneknemec@outlook.cz>
2023-11-29 13:59:26 +0100

    chore(Pages:Home): add easter egg

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,6 +3,20 @@ import logo from "../../images/logos/logo.png";
 import React from "react";
 
 export default function Home() {
+
+    console.log("\n\n{\\__/}\n" +
+                "( • - •)\n" +
+                "💶< \\  u want this money? spend it wisely though okay, good");
+    console.log("\n\n{\\__/}\n" +
+                "( • .•)\n" +
+                "/ >💶 alright here");
+    console.log("\n\n{\\__/}\n" +
+                "( o .o)  💸\n" +
+                "/ > >");
+    console.log("\n\n{\\__/}\n" +
+                "( ò .ó)\n" +
+                "/ > >  the fuck did I just say-");
+
     return (
         <div className="home-container">
             <center>
