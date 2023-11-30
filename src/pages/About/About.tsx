bc675f8b601add30cf517155bc36503358abe892
Zdeněk Němec <xzdeneknemec@outlook.cz>
2023-11-30 13:38:05 +0100

    chore(Pages): edit styles for career. restyle about

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,34 +1,23 @@
 import './About.scss';
+import logo from "../../images/logos/logo.png";
+import React from "react";
+import {LoremIpsum} from "../../components";
 
 export default function About() {
     return (
         <div className="about-container">
-            <div className="about-bg"><h4>Od profesionálů pro profesionály</h4></div>
+            <div className="about-bg"><p><b>KDO JSME?</b></p></div>
 
-            <div className="about-content">
-                <div className="about-content-text">
-                    <p>Naše firma vznikla v roce 1996 a již od této doby se <b>odborně věnujeme distribuci a především poradenství ohledně profesionálních nástrojů pro zpracování dřeva</b>, materiálů na bázi dřeva a plastů od renomovaných výrobců.</p>
+            <div className="first-part">
+                <div className="first-part-row">
+                    <img src={logo} alt="logo" className="about-text-logo first-part-row-content" width='400' height='auto'/>
+                    <span className="first-part-row-content"><LoremIpsum/></span>
                 </div>
-
-                <div className="about-content-text">
-                    <p>Již roky <b className="">spolupracujeme s velkým počtem odborných firem</b> zabývajících se zpracováním dřeva, a to nejen v České republice, ale v celé Evropě.</p>
-                    <p>Poslední <b className="">roky expandujeme </b>a snažíme se pro Vás zlepšovat náš servis a dostupnost, abychom Vám mohli být na blízko v každé situaci.</p>
+                <div className="first-part-row">
+                    <span className="first-part-row-content"><LoremIpsum/></span>
+                    <img src={logo} alt="logo" className="about-text-logo first-part-row-content" width='400' height='auto'/>
                 </div>
-
-                <div className="about-content-text">
-                    <p>V reakci na tržní poptávku <b>neustále rozšiřujeme naši nabídku</b>, abychom našim klientům pomohli <b>optimalizovat výrobní proces a snížit jeho náklady</b>. Na zvláštní žádost poskytujeme také nástroje s <b>neobvyklými rozměry a aplikacemi</b>.</p>
-                    <p>Naši <b>kvalifikovaní zaměstnanci</b> poskytují <b>technické poradenství</b> při výrobě vhodných nástrojů a jejich údržbě.</p>
-                </div>
-
-                <div className="about-content-text">
-                    <p>Po celá léta se <b>aktivně účastníme domácích a zahraničních veletrhů</b> a navazujeme obchodní kontakty, které nám umožňují dynamicky rozvíjet prodej nejen v <b>Polsku, ale také třeba v České republice</b> či jiných státech.</p>
-                </div>
-
-                <div className="about-content-text">
-                    <p><b>Nejvýznamnější událostí</b> každého roku je <b>mezinárodní veletrh strojů a nářadí DREMA</b> pro dřevařský a nábytkářský průmysl v Poznani. Jsme hrdí na to, že můžeme představit další <b>nové značky</b>, které se připojily k našemu portfoliu produktů.</p>
-                    <p>Již několik let je náš stánek <b>oceněn cenou Acanthus Aureus</b>. Soška se uděluje subjektům, jejichž postavení <b>nejlépe odráží marketingovou strategii společnosti</b>.</p>
-                </div>
-
+                <LoremIpsum repeat={2}/>
             </div>
         </div>
     )
