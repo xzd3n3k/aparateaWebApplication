d1bd99b7e1e0ffcdf3ac4a3396a2682fbf2b29a7
Zdeněk Němec <xzdeneknemec@outlook.cz>
2023-12-24 13:43:58 +0100

    add user account type model, add new global style

---
+++ src/TAccount.ts
@@ -0,0 +1,11 @@
+type TAccount = {
+    id: number;
+    username: string;
+    email: string;
+    first_name: string;
+    last_name: string;
+    phone: string;
+    linked_company: number;
+};
+
+export default TAccount;
