0a52271dd7a815b84f9beb739c649b97e0cc2156
Zdeněk Němec <xzdeneknemec@outlook.cz>
2023-12-25 21:21:08 +0100

    complete add user function, add generate random acc option

--- src/pages/Sharpening/pages/Settings/Settings.tsx
+++ src/pages/Sharpening/pages/Settings/Settings.tsx
@@ -5,10 +5,12 @@ import { Accounts } from "../index";
 import { Modal } from "../../../../components";
 import { toast, ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
+import api from "../../../../api";
 
 export default function Settings(): ReactElement {
 
     const [showModal, setShowModal] = useState(false);
+    const [showCredentials, setShowCredentials] = useState(false);
 
     const openModal = () => {
         setShowModal(true);
@@ -17,11 +19,6 @@ export default function Settings(): ReactElement {
     const closeModal = () => {
         setShowModal(false);
     };
-
-    // const confirmModal = () => {
-    //     toast.success(`Uživatel úspěšně přidán`);
-    //     setShowModal(false);
-    // };
 
     const [selectedOption, setSelectedOption] = useState("users");
 
@@ -58,12 +55,19 @@ export default function Settings(): ReactElement {
     const title: string = modalTitle();
     const btnText: string = buttonText();
 
-    // program to generate random strings
-
-    const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-
     function generatePassword(length: number) {
+        const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        let result = ' ';
+        const charactersLength = characters.length;
+        for ( let i = 0; i < length; i++ ) {
+            result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
 
+        return result;
+    }
+
+    function generatePhoneNumber(length: number) {
+        const characters ='0123456789';
         let result = ' ';
         const charactersLength = characters.length;
         for ( let i = 0; i < length; i++ ) {
@@ -75,26 +79,103 @@ export default function Settings(): ReactElement {
 
     const [generateRandom, setGenerateRandom] = useState(false);
 
+    const [username, setUsername] = useState<string>("");
+    const [firstName, setFirstName] = useState<string>('');
+    const [lastName, setLastName] = useState<string>('');
+    const [email, setEmail] = useState<string>('');
+    const [phone, setPhone] = useState<string>("");
+    const [password, setPassword] = useState<string>('');
 
     const btnBody: ReactNode =
         <div className="d-flex flex-column gap-3">
             <span className="d-flex flex-row gap-3">
                 <label className="form-label">Generate random acc</label>
                 <input type="checkbox" id="genRandom" name="genRandom" className="form-check-input"
-                       onChange={() => setGenerateRandom(!generateRandom)} />
+                       onChange={(event) => {
+                           setGenerateRandom((prevGenerateRandom) => {
+                               if (event.target.checked) {
+                                   setPassword(generatePassword(15));
+                                   setEmail(`${generatePassword(6)}@gmail.com`);
+                                   setFirstName(generatePassword(6));
+                                   setLastName(generatePassword(6));
+                                   setUsername(generatePassword(6));
+                                   setPhone(generatePhoneNumber(9));
+
+                                   return true;
+                               } else {
+                                   return false;
+                               }
+                           });
+                       }} />
             </span>
-            <input disabled={generateRandom} type="text" className="form-control" placeholder="Uživatelské jméno (volitelné)" />
-            <input disabled={generateRandom} type="text" className="form-control" placeholder="Jméno" />
-            <input disabled={generateRandom} type="text" className="form-control" placeholder="Příjmení" />
-            <input disabled={generateRandom} type="email" className="form-control" placeholder="Email" />
-            <input disabled={generateRandom} type="text" className="form-control" placeholder="Mobil (volitelné)" />
-            <input disabled={generateRandom} type="password" className="form-control" placeholder="Heslo" />
+            <input onChange={event => setUsername(event.target.value)} disabled={generateRandom} type="text" className="form-control" placeholder="Uživatelské jméno (volitelné)" />
+            <input onChange={event => setFirstName(event.target.value)} disabled={generateRandom} type="text" className="form-control" placeholder="Jméno" />
+            <input onChange={event => setLastName(event.target.value)} disabled={generateRandom} type="text" className="form-control" placeholder="Příjmení" />
+            <input onChange={event => setEmail(event.target.value)} disabled={generateRandom} type="email" className="form-control" placeholder="Email" />
+            <input onChange={event => setPhone(event.target.value)} disabled={generateRandom} type="text" className="form-control" placeholder="Mobil (volitelné)" />
+            <input onChange={event => setPassword(event.target.value)} disabled={generateRandom} type="password" className="form-control" placeholder="Heslo" />
         </div>
 
-    const confirmModal = () => {
+    const credentialsBody: ReactNode =
+        <div>
+            <div className="d-flex flex-column gap-2">
+                <label>Uživatelské jméno</label>
+                <p>{username}</p>
+            </div>
+            <div className="d-flex flex-column gap-2">
+                <label>Jméno</label>
+                <p>{firstName} {lastName}</p>
+            </div>
+            <div className="d-flex flex-column gap-2">
+                <label>Email</label>
+                <p>{email}</p>
+            </div>
+            <div className="d-flex flex-column gap-2">
+                <label>Mobil</label>
+                <p>{phone}</p>
+            </div>
+            <div className="d-flex flex-column gap-2">
+                <label>Heslo</label>
+                <p>{password}</p>
+            </div>
+        </div>;
+
+    const confirmModal = async () => {
         switch (selectedOption) {
             case "users":
-                toast.success(`Uživatelský účet vytvořen`);
+
+                const response = await fetch(`${api}/register`, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({
+                        "password": password,
+                        "email": email,
+                        "first_name": firstName,
+                        "last_name": lastName,
+                        "username": username,
+                        "phone": phone
+                    }),
+                });
+
+                const result = await response.json();
+
+                if (response.status === 200) {
+                    if (result === 'User already exists!') {
+                        toast.error('Uživatelský účet již existuje!');
+
+                    } else {
+                        if (generateRandom) {
+                            setShowCredentials(true);
+                        }
+                        toast.success(`Uživatelský účet vytvořen`);
+                    }
+
+                } else {
+                    toast.error("Chyba při vytváření účtu!");
+                }
+
                 setShowModal(false);
                 break;
             case "customers":
@@ -132,6 +213,7 @@ export default function Settings(): ReactElement {
     return (
         <div className="settings-container d-flex flex-column w-100 p-4 gap-3">
             <Modal isOpen={showModal} handleClose={closeModal} handleConfirm={confirmModal} title={title} body={btnBody} confirmButtonText={btnText} buttonColor="btn-primary" />
+            <Modal isOpen={showCredentials} handleClose={() => {setShowCredentials(false)}} handleConfirm={() => {setShowCredentials(false)}} title="Údaje k účtu" body={credentialsBody} confirmButtonText="OK" buttonColor="btn-secondary"/>
             <div className="d-flex flex-row justify-content-between">
             <button className="btn btn-light" onClick={() => {window.location.href="/sharpening/records"}}>
                 <img src={arrowLeft} alt="Back" width="24" height="24" />
