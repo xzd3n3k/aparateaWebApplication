cad1fff16a7b4ca3affacaae1327dc5f6ff14172
Zdeněk Němec <xzdeneknemec@outlook.cz>
2023-12-29 22:38:51 +0100

    create TCompany type

---
+++ src/TCompany.ts
@@ -0,0 +1,17 @@
+type TCompany = {
+    id: number;
+    name: string;
+    state: string;
+    town: string;
+    street: string;
+    cislo_popisne: string;
+    psc: string;
+    phone: string;
+    email: string;
+    ic: string;
+    dic: string;
+    executive: string;
+    note: string;
+};
+
+export default TCompany;
