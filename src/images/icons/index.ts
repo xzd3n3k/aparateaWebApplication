8343b6203987ef5d843443e8b3880c416b838e97
Zdeněk Němec <xzdeneknemec@outlook.cz>
2024-01-09 13:25:54 +0100

    add icons

--- src/images/icons/index.ts
+++ src/images/icons/index.ts
@@ -3,3 +3,5 @@ export { default as arrowLeft } from './arrow-left.svg';
 export { default as x } from './x.svg';
 export { default as pencil} from './pencil.svg';
 export { default as plus} from './plus.svg';
+export { default as check } from './check.svg';
+export { default as arrowRight } from './arrow-right.svg';
